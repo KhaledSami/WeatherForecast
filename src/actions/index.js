126b682dc775b64c5513773cc3106aa4abfc22e6
Khaled Sami <k.sami.mohammed@gmail.com>
2017-04-09 15:50:34 +0100

    Add the logic for weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 const API_KEY = '27894c6235020739c33ab0f798fe1bd9';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FEATCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetechWeather(city) {
 	const url = `${ROOT_URL}&q=${city}`;
 	const request = axios.get(url);
 	console.log(url);
 	return {
-		type: FEATCH_WEATHER,
+		type: FETCH_WEATHER,
 		payload: request
 	}
 }
